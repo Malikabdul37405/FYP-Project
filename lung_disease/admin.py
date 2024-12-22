d8da6e49e5d51a9db7d6414e4cc6f09b4e51005b
Malikabdul37405 <Malikabdul37405@example.com>
2024-12-22 17:17:13 +0500

    footer adjustment

--- lung_disease/admin.py
+++ lung_disease/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import UploadedImage, Blog, ContactInfo
 
+admin.site.site_header = "Lung Disease Admin Site"  # Title for the admin panel header
+admin.site.site_title = "Lung Disease App"         # Title for the browser tab
+admin.site.index_title = "Welcome to the Lung Disease App Admin Panel"  # Subtitle on the admin index page
+
 class UploadedImageAdmin(admin.ModelAdmin):
     list_display = ('user', 'image', 'uploaded_at')  # Fields to display in the admin panel
     search_fields = ('user__username',)             # Search functionality by username
